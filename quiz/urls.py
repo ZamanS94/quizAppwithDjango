efe18a5e3e87757b55782cdcc94a1ae51c0951a9
ZamanS94 <106835600+ZamanS94@users.noreply.github.com>
2026-06-16 23:36:51 +0300

    Update urls.py

--- quiz/urls.py
+++ quiz/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('submit/<int:question_id>/', views.submit_answer, name='submit_answer'),
     path('leaderboard/', views.leaderboard, name='leaderboard'),
     path('signup/', views.signup, name='signup'),
+    path('my-predictions/', views.my_predictions, name='my_predictions'),
 ]
