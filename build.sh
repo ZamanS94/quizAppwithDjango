52c508d035e14b70a8e632daa42d075027cd45db
ZS94 <sabina.zaman94@gmail.com>
2026-06-16 23:10:23 +0300

    Add migrations

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt && python manage.py migrate && gunicorn config.wsgi
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('Zaman', 'sabina.zaman@gmail.com', 'sN&071158')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
