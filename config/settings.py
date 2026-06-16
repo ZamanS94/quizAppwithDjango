05429bfdc02202784ab09ddc7f62cb7198840da2
ZamanS94 <106835600+ZamanS94@users.noreply.github.com>
2026-06-16 22:49:37 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -70,9 +70,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=not DEBUG,
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
     )
 }
 
