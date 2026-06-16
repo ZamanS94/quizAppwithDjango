177b5d183a242fcb4f69387be8abe64835b386bb
ZamanS94 <106835600+ZamanS94@users.noreply.github.com>
2026-06-16 23:34:35 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ pip install -r requirements.txt
 python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('Zaman', 'sabina.zaman@gmail.com', 'sN&071158')
+import os
+if not User.objects.filter(username='Zaman').exists():
+    User.objects.create_superuser('Zaman', 'sabina.zaman@gmail.com', os.environ.get('SUPERUSER_PASSWORD'))
     print('Superuser created')
 else:
     print('Superuser already exists')
